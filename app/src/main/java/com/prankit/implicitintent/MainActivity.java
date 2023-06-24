17f98306324f102348ae2bc31157bea25c3ba58b
agrwl-prankit <agrwnlprankit77@gmail.com>
2023-06-24 05:29:13 -0400

    implemented implicit intent

--- app/src/main/java/com/prankit/implicitintent/MainActivity.java
+++ app/src/main/java/com/prankit/implicitintent/MainActivity.java
@@ -2,7 +2,11 @@ package com.prankit.implicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.goToButton);
+        button.setOnClickListener(v -> {
+            // implicit intent
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.google.com"));
+            startActivity(intent);
+        });
     }
 }
